c13ec85a46b3ef5c40593467e4ba7419e8233006
Nicola <n@scc.im>
2026-02-20 10:00:00 +0100

    PricingEngine: add pharmacy markup

--- app/services/pricing_engine.rb
+++ app/services/pricing_engine.rb
@@ -6,6 +6,14 @@ class PricingEngine
 
   def calculate
     base = @product.price_cents
+    base = apply_pharmacy_markup(base) if @pharmacy
     { final_price: base, currency: "EUR" }
+  end
+
+  private
+
+  def apply_pharmacy_markup(price)
+    markup = @pharmacy.auto_accept_orders? ? 1.05 : 1.10
+    (price * markup).round
   end
 end
